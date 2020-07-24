2c74d4b27c06b4fa2e74458d9745e63df00dbfdf
wuzifeng <wuzifeng@bluemoon.com.cn>
2020-07-24 15:05:41 +0800

    complete develop 1.0.0

---
+++ src/main/java/cn/com/bluemoon/bd/java/demo/DemoApplication.java
@@ -0,0 +1,15 @@
+package cn.com.bluemoon.bd.java.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: 吴梓锋
+ * @Date: 2020/7/22 14:05:25
+ */
+@SpringBootApplication
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
